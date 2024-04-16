47df2b8fab118e597756944530167d25c369f22a
25042 <2504225565@qq.com>
2024-04-16 16:54:52 +0800

    fix path

--- .history/app_20240416164745.py
+++ .history/app_20240416164745.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2-chat-7b'
+base_path = './internlm2-chat-7B'
 os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7B.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
